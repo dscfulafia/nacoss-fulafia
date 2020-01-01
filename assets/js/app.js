a6261ee5898b66460c7c22582114f51d476e218e
dscfulafia <55881696+dscfulafia@users.noreply.github.com>
2020-01-01 15:05:54 -0800

    added axios call

---
+++ assets/js/app.js
@@ -0,0 +1,38 @@
+var API_URL = "http://nacoss-ful.000webhostapp.com/api/";
+
+var example1=new Vue({
+  el: '#post',
+  data () {
+    return {
+      posts: null,
+      loading: true,
+      errored: false
+    }
+  },
+  methods: {
+    shortenedPostContent (value) {
+        //stripping any html content in the value
+        var htmlContent = value
+        var virtualDiv = document.createElement("div")
+        virtualDiv.innerHTML = value
+      return virtualDiv.innerText.substring(0,300)+"....";
+    },
+    getImage (imageURL) {
+      return (imageURL == null) ? "assets/gallery/no-image.jpg" : API_URL+"../wordpress/wp-content/uploads/"+imageURL;
+    }
+
+  },
+  mounted () {
+    axios
+      .get(API_URL + '?call=getallposts')
+      .then(response => {
+        this.posts = response.data.data.body
+        this.errored =  response.data.data.errorExist
+      })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
+  }
+})
---
+++ assets/js/post.js
@@ -0,0 +1,34 @@
+var thisUrl=window.location.href;
+var url=new URL(thisUrl);
+var page_id=url.searchParams.get("id");
+
+var API_URL = "http://nacoss-ful.000webhostapp.com/api/";
+
+var example1=new Vue({
+  el: '#post',
+  data () {
+    return {
+      post: null,
+      loading: true,
+      errored: false
+    }
+  },
+  methods: {
+    getImage (imageURL) {
+      return (imageURL == null) ? "assets/gallery/no-image.jpg" : API_URL+"../wordpress/wp-content/uploads/"+imageURL;
+    }
+  },
+  mounted () {
+    axios
+      .get(API_URL + '?call=getpost&id='+page_id)
+      .then(response => {
+        this.post = response.data.data.body
+        this.errored =  response.data.data.errorExist
+      })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
+  }
+})
